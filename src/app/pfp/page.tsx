5fa125dd8ebd458967e1a9eb8023f27bae9190c8
VPIS <74914934+VPIS@users.noreply.github.com>
2025-09-03 18:54:15 -0400

    Update page.tsx

--- src/app/pfp/page.tsx
+++ src/app/pfp/page.tsx
@@ -1,5 +1,6 @@
 
 import LogoBox from "@/components/LogoBox";
+import ColorBox from "@/components/ColorBox";
 import LogoCategory from "@/components/LogoCategory";
 import {Metadata} from "next";
 
@@ -62,6 +63,12 @@ export default function Home() {
                     <LogoBox name={"People for People Logo Vertical White"} downloads={logos["p4p-icon-white"]} background={"purple"} buttonColor={"white"} size={"small"}/>
                     <LogoBox name={"People for People Logo Vertical Black"} downloads={logos["p4p-icon-black"]} background={"white"} buttonColor={"black"} size={"small"}/>
                 </LogoCategory>
+
+                <div className={`flex-col md:flex-row bg-white p-5 rounded-md border-2 border-gray-300 md:space-x-5 space-y-5 md:space-y-0 hidden md:flex`}>
+                    <ColorBox variant={"p4p-purple"}/>
+                    <ColorBox variant={"teal"}/>
+                    <ColorBox variant={"orange"}/>
+                </div>
                 
             </div>
         </div>
